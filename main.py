ef545e39f87495cfb76d572c06bf5e3970fd77bd
Ishaan Ivaturi <ishaan.a.ivaturi@gmail.com>
2020-08-26 17:50:17 -0400

    Converted test to full echo, prefix = !, comments

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from configuration import config
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix=commands.when_mentioned, help_command=None)
+client = commands.Bot(command_prefix='!', help_command=None)
+
 
 @client.event
 async def on_ready():
-    print(f'Logged on as {self.user}')
+    """Prints message to terminal when bot is ready"""
+    #Uses client.user to output name and id of bot
+    print(f'Logged on as {client.user}')
+
 
 @client.command()
-async def test(ctx, content):
-    await ctx.send(content)
+async def echo(ctx, *args):
+    """Takes in space separated arguments and outputs those same arguments"""
+    #Uses join command to space separate args, no matter how many there are
+    await ctx.send(' '.join(args))
 
 client.run(config["botToken"])
