152627af651d9a118038f74acc55db2a0b384937
Ishaan Ivaturi <ishaan.a.ivaturi@gmail.com>
2020-08-30 21:49:11 -0400

    Added help command using embeds

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 class TestCog(commands.Cog):
@@ -16,6 +17,19 @@ class TestCog(commands.Cog):
         """Takes in space separated arguments and outputs those same arguments"""
         #Uses join command to space separate args, no matter how many there are
         await ctx.send(' '.join(args))
+
+    
+    @commands.command()
+    async def help(self, ctx):
+        """Displays information about all the commands in the bot"""
+        #Create an embed with information on each command
+        embed = discord.Embed(title="Help", description="RUCS24 Commands", color=0xff0000)
+        embed.add_field(name="!echo", value="Params: Any Phrase\nDescription: Repeats your phrase word for word", inline=False)
+        embed.add_field(name="!open", value="Params: Section Index\nDescription: Tells you whether a given course section is open or closed currently", inline=False)
+        embed.add_field(name="!tictactoe", value="Params: None\nDescription: Starts a game of tictactoe with the player", inline=False)
+
+        #Send the embed
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
