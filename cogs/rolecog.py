ebb5ded23eda52c2bd2083250664b9f77f62aa73
Ambika Nair <cepheac@protonmail.ch>
2020-08-31 03:05:27 -0400

    Added some documentation

--- cogs/rolecog.py
+++ cogs/rolecog.py
@@ -19,12 +19,20 @@ def read_from_config(key):
 
 class RoleCog(commands.Cog):
     def __init__(self, bot):
+        """Initialise the bot and draw important values from the configuration file"""
+
         self.bot = bot
         self._roles_config = read_from_config('roles')
         self._selector_channel_id = self._roles_config['selectorChannel']
         self._listeners = self._roles_config['listeners']
 
-    async def add_remove_role(self, payload, add): # add is a boolean denoting whether or not this is an add or removal
+    async def add_remove_role(self, payload, add):
+        """Add or remove a role in response to a reaction to a specified message in a specified channel
+
+        Args:
+            payload: A discord.RawReactionEvent object representing the reaction or reaction removal
+            add: A boolean denoting whether or not the event being fired is a reaction being added"""
+
         selector_channel = self.bot.get_channel(self._selector_channel_id)
         if selector_channel is None:
             print(f"Error: No valid selector channel was specified.") 
@@ -33,7 +41,9 @@ class RoleCog(commands.Cog):
         message = await selector_channel.fetch_message(payload.message_id)
         guild = selector_channel.guild
         member = guild.get_member(payload.user_id)
-        
+
+        # Grabbing a list of lists of valid emojis from the config file and then flattening it into one single list
+        # Learn more about the map and lambda syntax here: https://medium.com/better-programming/lambda-map-and-filter-in-python-4935f248593
         valid_emoji_lists = map(lambda listener: listener.keys(), self._listeners.values())
         valid_emojis = [item for sublist in valid_emoji_lists for item in sublist]
         
@@ -43,7 +53,7 @@ class RoleCog(commands.Cog):
                 await member.add_roles(corresponding_role)
             elif corresponding_role in member.roles and not add:
                 await member.remove_roles(corresponding_role)
-        elif add:
+        elif add: # In the case that someone reacted with an emoji that doesn't correspond to a role
             await message.remove_reaction(payload.emoji, member)
         return
 
