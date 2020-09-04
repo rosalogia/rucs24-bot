34240b0173fe544d76be237b6ff5912bf48a2589
Ambika Nair <cepheac@protonmail.ch>
2020-09-03 22:30:52 -0400

    Reformatted code to adhere to PEP8 with black

--- cogs/apicog.py
+++ cogs/apicog.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import requests
 import json
 
+
 class ApiCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,26 +11,26 @@ class ApiCog(commands.Cog):
     async def hi(self, ctx):
         """Says hi"""
         return
-    
+
     @commands.command()
     async def open(self, ctx, index):
         """Takes in course section index and outputs whether the section is open or closed"""
 
-        #Request URL to retrieve indexes of all open sections currently
+        # Request URL to retrieve indexes of all open sections currently
         open_sections_url = "https://sis.rutgers.edu/soc/api/openSections.gzip?year=2020&term=9&campus=NB"
 
-        #Try catch in case the api call fails
+        # Try catch in case the api call fails
         try:
-            #I load the text from requests call into a list called open_sections
-            #open_sections now contains indexes of all open sections
+            # I load the text from requests call into a list called open_sections
+            # open_sections now contains indexes of all open sections
             open_sections = json.loads(requests.get(open_sections_url).text)
         except Exception as e:
-            #If something went wrong, output API error and exit function
+            # If something went wrong, output API error and exit function
             await ctx.send("API Error, sorry!")
             print(e)
             return
-        
-        #Check if index entered is in open sections, output the proper statement
+
+        # Check if index entered is in open sections, output the proper statement
         if index in open_sections:
             await ctx.send(f"Section {index} is currently open!")
         else:
