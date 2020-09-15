91878d127b127aef19302fc70eaa36b14eb49333
Ishaan Ivaturi <ishaan.a.ivaturi@gmail.com>
2020-09-15 03:41:22 -0400

    Help command is now categorized by cog

--- cogs/corecog.py
+++ cogs/corecog.py
@@ -27,12 +27,25 @@ class CoreCog(commands.Cog):
             title="Help", description="RUCS24 Commands", color=0xFF0000
         )
 
+        #Stores cog name and maps to all the functions
+        cogs_dict = {}
+
         # Go through each command, access its name and docstring, add to embed
         for func in self.bot.walk_commands():
-            embed.add_field(name="!" + func.name, value=func.help, inline=False)
+            #Adds the function to cogs_dict based on its cog
+            try:
+                cogs_dict[func.cog_name].append((func.name, func.help))
+            except:
+                cogs_dict[func.cog_name] = [(func.name, func.help)]
 
-        # Finally, send the embed
-        await ctx.send(embed=embed)
+        #Create the cog, add all the commands, and send the embed
+        for cog in cogs_dict.keys():
+            embed = discord.Embed(title=cog, description = "Help", color=0xFF0000)
+
+            for command in cogs_dict[cog]:
+                embed.add_field(name="!" + command[0], value = command[1], inline=False)
+            
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
