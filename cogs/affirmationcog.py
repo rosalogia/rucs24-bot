037354bd887a09c3fc958a7550df4470b2407ed1
Poppamunz <poppaworld@gmail.com>
2020-10-06 23:27:48 -0400

    PEP8 formatting

--- cogs/affirmationcog.py
+++ cogs/affirmationcog.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 class AffirmationCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def affirmation(self, ctx):
         """Tells you a positive affirmation"""
@@ -14,6 +14,6 @@ class AffirmationCog(commands.Cog):
         apidata = r.json()
         await ctx.send(apidata["affirmation"])
 
+
 def setup(bot):
     bot.add_cog(AffirmationCog(bot))
-        
