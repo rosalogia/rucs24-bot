e6d9e8f245a6d7d1d76104df0e0db4d741f99936
Arihant Tripathi <tarihant2001@gmail.com>
2020-10-10 22:56:44 -0400

    Added avatarcog (#68)

--- cogs/statscog.py
+++ cogs/statscog.py
@@ -3,7 +3,6 @@ import logging
 import discord
 from discord.ext import commands, tasks
 
-
 class StatsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,10 +13,10 @@ class StatsCog(commands.Cog):
         dpyversion = discord.__version__
         serverCount = len(self.bot.guilds)
         memberCount = len(set(self.bot.get_all_members()))
+
         await ctx.send(
             f"So im in {serverCount} guilds with a total of {memberCount} members. :smiley:\nIm running python {pythonVersion} and discord.py {dpyversion}"
         )
-
 
 def setup(bot):
     bot.add_cog(StatsCog(bot))
