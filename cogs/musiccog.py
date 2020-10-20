27735d9247ca97a66c70164cad7d74df367dc955
Ambika Eshwar <ambika.eshwar@rutgers.edu>
2020-10-20 04:55:22 -0400

    Added docstrings to music cog

--- cogs/musiccog.py
+++ cogs/musiccog.py
@@ -6,12 +6,14 @@ import youtube_dl
 from youtube_search import YoutubeSearch
 import validators
 
+
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True, aliases=["J", "jo"])
     async def join(self, ctx):
+        """Have the bot join the voice channel to which you're currently connected"""
         global voice
         channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild=ctx.guild)
@@ -30,9 +32,9 @@ class MusicCog(commands.Cog):
         else:
             voice = await channel.connect()
 
-
     @commands.command(pass_context=True, aliases=["L", "le"])
     async def leave(self, ctx):
+        """Have the bot leave the voice channel to which it's currently connected"""
         channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild=ctx.guild)
 
@@ -43,8 +45,20 @@ class MusicCog(commands.Cog):
 
     @commands.command(pass_context=True, aliases=["p", "pl"])
     async def play(self, ctx, url: str):
+        """Have the bot play the audio from a provided youtube link.
+        If no valid url is provided, the bot will interpret the input
+        as a search term and find the first result for the given input.
+
+        If a search term contains more than one word, make sure to wrap
+        the search term in quotes.
+
+        Usage: `!play <youtube_link | search_term>`
+        """
         if not validators.url(url):
-            url = "https://youtube.com" + YoutubeSearch(url, max_results=1).videos[0]["url_suffix"]
+            url = (
+                "https://youtube.com"
+                + YoutubeSearch(url, max_results=1).videos[0]["url_suffix"]
+            )
 
         song_there = os.path.isfile("./data/song.mp3")
         try:
@@ -77,7 +91,11 @@ class MusicCog(commands.Cog):
         for file in os.listdir("./data/"):
             if file.endswith("mp3"):
                 name = " ".join(file.split(".")[:-1])
-                await self.bot.change_presence(activity=discord.Activity(name=name, type=discord.ActivityType.listening))
+                await self.bot.change_presence(
+                    activity=discord.Activity(
+                        name=name, type=discord.ActivityType.listening
+                    )
+                )
                 os.rename(f"./data/{file}", "song.mp3")
 
         voice.play(discord.FFmpegPCMAudio("song.mp3"))
@@ -88,6 +106,7 @@ class MusicCog(commands.Cog):
 
     @commands.command(pass_context=True, aliases=["pa", "pau"])
     async def pause(self, ctx):
+        """Pause currently playing music"""
         voice = get(self.bot.voice_clients, guild=ctx.guild)
 
         if voice and voice.is_playing():
@@ -98,6 +117,7 @@ class MusicCog(commands.Cog):
 
     @commands.command(pass_context=True, aliases=["re", "resum"])
     async def resume(self, ctx):
+        """Resume playback of music that has been paused"""
         voice = get(self.bot.voice_clients, guild=ctx.guild)
 
         if voice and voice.is_paused():
@@ -108,6 +128,7 @@ class MusicCog(commands.Cog):
 
     @commands.command(pass_context=True, aliases=["st", "stp"])
     async def stop(self, ctx):
+        """Stop the playback of music that is currently playing"""
         voice = get(self.bot.voice_clients, guild=ctx.guild)
         if voice and voice.is_playing():
             voice.stop()
